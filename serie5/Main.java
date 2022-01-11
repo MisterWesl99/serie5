7555d2f030f34476c9f61cad81f1c2419e09db0d
Marek Becher <67748842+marek010@users.noreply.github.com>
2022-01-11 15:28:13 +0100

    final version

--- serie5/Main.java
+++ serie5/Main.java
@@ -5,9 +5,8 @@ import java.util.List;
 import java.io.Writer;
 import java.io.FileWriter;
 
-//known error: verschiebe Buchstaben evtl. Fehler mit negativ Wert und tausche Zeichen hat kein Einlfuss auf Output
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Leser leser = new Leser("trag1igkel.txt.gz");
 
         if ("serie5 encryption".equals(leser.getHeader())) {
@@ -15,26 +14,28 @@ public class Main {
             List<String> inhalt = leser.liestInhalt();
 
             Verschluesselung.tauscheZeilen(inhalt);
-            Writer fileWriter = new FileWriter("AuszuegeGG.txt");
-            for (int i = 0; i < inhalt.size(); i++) {
-                inhalt.set(i, Verschluesselung.verschiebeBuchstaben(inhalt.get(i), -13));
-                inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), 'r', 'g'));
-                inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), 'a', 't'));
-                inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), ')', '('));
-                inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), ',', '.'));
+            try (Writer fileWriter = new FileWriter("AuszuegeGG.txt")) {
+                for (int i = 0; i < inhalt.size(); i++) {
+                    inhalt.set(i, Verschluesselung.verschiebeBuchstaben(inhalt.get(i), -13));
+                    inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), 'r', 'g'));
+                    inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), 'a', 't'));
+                    inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), ')', '('));
+                    inhalt.set(i, Verschluesselung.tauscheZeichen(inhalt.get(i), ',', '.'));
 
-                fileWriter.write(inhalt.get(i));
+                    fileWriter.write(inhalt.get(i));
 
-                // System.out.println(inhalt.get(i));
+                }
+
+                fileWriter.close();
+            } catch (IOException e) {
+
+                e.printStackTrace();
             }
-
-            fileWriter.close();
         } else {
-            System.out.println('f');
-            // add system.err
+            System.err.println("Header not matching expectations.");
 
         }
-        System.out.println("done");
+
     }
 
 }
